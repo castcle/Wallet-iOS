90b859ea51629b2bf520555834cf3f835a93f864
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2022-05-11 10:08:34 +0700

    Init project

---
+++ Wallet/Wallet.h
@@ -0,0 +1,18 @@
+//
+//  Wallet.h
+//  Wallet
+//
+//  Created by Tanakorn Phoochaliaw on 11/5/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Wallet.
+FOUNDATION_EXPORT double WalletVersionNumber;
+
+//! Project version string for Wallet.
+FOUNDATION_EXPORT const unsigned char WalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wallet/PublicHeader.h>
+
+
