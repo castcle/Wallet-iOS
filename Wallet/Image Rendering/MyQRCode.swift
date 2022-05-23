ec4e42299e300808da91cd3ea3aa9fcbb30a266b
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-05-23 16:35:50 +0700

    Poc func generate qr code and create view to image

---
+++ Wallet/Image Rendering/MyQRCode.swift
@@ -0,0 +1,64 @@
+//  Copyright (c) 2021, Castcle and/or its affiliates. All rights reserved.
+//  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+//  This code is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 only, as
+//  published by the Free Software Foundation.
+//
+//  This code is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+//  version 3 for more details (a copy is included in the LICENSE file that
+//  accompanied this code).
+//
+//  You should have received a copy of the GNU General Public License version
+//  3 along with this work; if not, write to the Free Software Foundation,
+//  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//  Please contact Castcle, 22 Phet Kasem 47/2 Alley, Bang Khae, Bangkok,
+//  Thailand 10160, or visit www.castcle.com if you need additional information
+//  or have any questions.
+//
+//  MyQRCode.swift
+//  Wallet
+//
+//  Created by Castcle Co., Ltd. on 23/5/2565 BE.
+//
+
+import UIKit
+import Core
+
+class MyQRCode: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var qrCodeImage: UIImageView!
+
+    // MARK: - How to generate and save qr code to device
+//    if let qrCodeImage = Utility.generateQRCode(from: "https://www.castcle.com") {
+//        let myQRCode: MyQRCode = MyQRCode(frame: CGRect(x: 0, y: 0, width: 400, height: 300))
+//        myQRCode.titleLabel.text = "Test My QR Code"
+//        myQRCode.qrCodeImage.image = qrCodeImage
+//        UIImageWriteToSavedPhotosAlbum(myQRCode.asImage(), self, nil, nil)
+//    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+
+    private func commonInit() {
+        ConfigBundle.wallet.loadNibNamed("MyQRCode", owner: self)
+        addSubview(self.contentView)
+        self.contentView.frame = self.bounds
+        self.contentView.backgroundColor = UIColor.Asset.white
+        self.titleLabel.font = UIFont.asset(.regular, fontSize: .body)
+        self.titleLabel.textColor = UIColor.Asset.black
+        self.contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
